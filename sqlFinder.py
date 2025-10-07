3bd322dbab685e5517bfe1f340ece12145ab1902
Faizuddin Shaik <faiz.24365@gmail.com>
2025-10-06 22:22:01 -0500

    create checks

--- sqlFinder.py
+++ sqlFinder.py
@@ -19,21 +19,26 @@ def sql_finder2(file_name):
     sqls = []
     with io.open(file_name, mode='r', encoding='utf-8') as f:
         for line in f:
+
             # Remove leading/trailing whitespace, including the newline character
             query_string = line.strip()
             comment = (query_string.startswith('-') or query_string.startswith('#') or query_string.startswith(
-                '/') or query_string.startswith('*'))
+                '/*') or query_string.startswith('*/'))
             # Check if the stripped line starts with the specified prefix
             if not comment:
+
                 processed_lines.append(query_string)
     sql = ''
     for word in processed_lines:
-        if not word.endswith(';'):
+
+        if not (word.endswith(';') or word == "/"):
+
             sql = sql + word.strip() + ' '
         else:
             sql = sql + word.strip()
             sqls.append(sql)
             sql = ''
+    # print(sqls)
     return sqls
 
 
@@ -55,18 +60,53 @@ def check_for_where(sql_string, keyword):
         raise ValueError(f"WHERE CLAUSE MISSING IN {keyword} query: '{sql_string}'")
 
 
+def check_for_create(list_of_queries, keyword,index):
+    query = list_of_queries[index]
+    table_name = query[len(keyword):].strip()
+    if '/' in table_name:
+        table_name = table_name.replace('/', '')
+    print("table_name: ", table_name)
+    string_to_check = 'CREATE TABLE '+ table_name
+    for query_string in list_of_queries[index:]:
+        if string_to_check in query_string.strip() :
+            valid_query = True
+            break
+        else:
+            valid_query = False
+    if not valid_query:
+        raise ValueError(f"CREATE TABLE {table_name} not found after {keyword} in the file.")
+
+
+
+
+def check_for_grant(sql_query, param):
+    pass
+
+
 def run_validation_rules():
     for filename in os.listdir(directory):
         if filename.endswith('.sql'):
+            # print(filename)
             filename = os.path.join(directory, filename)
             list_sqls = sql_finder2(filename)
+            # print(list_sqls)
             for sql_query in list_sqls:
+                sql_query = sql_query.strip()
                 # print(sql_query)
                 try:
-                    if sql_query.strip().startswith('UPDATE'):
+                    if sql_query.startswith('UPDATE'):
                         check_for_where(sql_query, 'UPDATE')
-                    elif sql_query.strip().startswith('DELETE'):
+                    elif sql_query.startswith('DELETE'):
                         check_for_where(sql_query, 'DELETE')
+                    elif sql_query.startswith('DROP TABLE'):
+                        if sql_query.startswith('DROP TABLE IF EXISTS'):
+                            index = list_sqls.index(sql_query)
+                            check_for_create(list_sqls,'DROP TABLE IF EXISTS',index)
+                        else:
+                            check_for_where(sql_query, 'DROP TABLE')
+                    elif sql_query.startswith('GRANT'):
+                        check_for_grant(sql_query, 'GRANT')
+                        
                     valid_sql = True
                 except Exception as e:
                     valid_sql = False
---
+++ sqls/test3.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS TBAADM.GAM
+/
+
+CREATE TABLE TBAADM.GAM (
+    column1 datatype,
+    column2 datatype,
+    column3 datatype,
+   ....
+)
+/
